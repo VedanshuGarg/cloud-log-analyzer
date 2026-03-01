4965e275039ed86e4e20f48d821ad9ca1623c323
Vedanshu Garg <garg.vedanshu04@gmail.com>
2026-03-01 18:27:33 +0530

    feat: project init with thread pool setup

---
+++ src/main/java/com/vedanshu/logs/Main.java
@@ -0,0 +1,22 @@
+package com.vedanshu.logs;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Starting Log Analyzer on " + cores + " cores...");
+
+        ExecutorService executor = Executors.newFixedThreadPool(cores);
+
+        for (int i = 1; i <= 5; i++) {
+            int taskId = i;
+            executor.submit(() -> {
+                System.out.println("Processing Chunk #" + taskId + " on Thread: " + Thread.currentThread().getName());
+            });
+        }
+
+        executor.shutdown();
+    }
+}
